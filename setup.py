52532cb5c90ec4c38fe7ac514d26dcf51a1cf825
ddomingof <danieldomingofernandez@hotmail.com>
2018-09-12 14:30:18 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -26,9 +26,7 @@ CLASSIFIERS = [
     'Topic :: Scientific/Engineering :: Bio-Informatics'
 ]
 INSTALL_REQUIRES = [
-    'networkx',
     'pandas',
-    'scipy',
     'tqdm',
     'numpy',
     'compath',
