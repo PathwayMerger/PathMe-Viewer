c65a7d82ab04dbe782c31955eaf35984b6298a7b
daniel.domingo.fernandez <danieldomingofernandez@hotmail.com>
2018-10-03 14:25:11 +0200

    Extra requirements missing in setup

--- setup.py
+++ setup.py
@@ -100,4 +100,5 @@ if __name__ == '__main__':
         package_dir={'': 'src'},
         include_package_data=True,
         install_requires=INSTALL_REQUIRES,
+        extras_require=EXTRAS_REQUIRE
     )
