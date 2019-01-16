569c7174d6d315658c027857777114c8161b92c4
daniel.domingo.fernandez <danieldomingofernandez@hotmail.com>
2019-01-16 21:37:09 +0100

    Bump version: 0.0.7 → 0.0.8-dev

--- src/pathme_viewer/__init__.py
+++ src/pathme_viewer/__init__.py
@@ -88,7 +88,7 @@ for entry_point in iter_entry_points(group=MODULE_NAME, name=None):
 
     managers[entry] = ExternalManager
 
-__version__ = '0.0.7'
+__version__ = '0.0.8-dev'
 
 __title__ = 'pathme_viewer'
 __description__ = "A plugin for PathMe that allows to explore overlaps across pathway databases."
